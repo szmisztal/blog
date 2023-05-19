b150a1186817f8ba9b31d3e4b1252ce1d7be2dbd
szmisztal <sz.misztal@gmail.com>
2023-05-19 12:18:41 +0200

    modify create_entry function to create_or_edit_entry function

--- blog/routes.py
+++ blog/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for, flash
 from blog import app
 from blog.models import Entry, db
 from blog.forms import EntryForm
@@ -8,19 +8,30 @@ def base():
     all_posts = Entry.query.filter_by(is_published = True).order_by(Entry.pub_date.desc())
     return render_template("homepage.html", all_posts = all_posts)
 
-@app.route("/new_post/", methods =["GET", "POST"])
-def create_entry():
-    form = EntryForm()
+@app.route("/new_post/", methods = ["GET", "POST"])
+@app.route("/edit_post/<int:entry_id>", methods = ["GET", "POST"])
+def create_or_edit_entry(entry_id = None):
+    if entry_id:
+        entry = Entry.query.filter_by(id = entry_id).first_or_404()
+        form = EntryForm(obj = entry)
+    else:
+        entry = None
+        form = EntryForm()
     errors = None
     if request.method == "POST":
         if form.validate_on_submit():
-            entry = Entry(
-                title = form.title.data,
-                body = form.body.data,
-                is_published = form.is_published.data
-            )
-            db.session.add(entry)
+            if entry:
+                form.populate_obj(entry)
+            else:
+                entry = Entry(
+                    title = form.title.data,
+                    body = form.body.data,
+                    is_published = form.is_published.data
+                )
+                db.session.add(entry)
             db.session.commit()
+            flash("Post saved !")
+            return redirect(url_for('base'))
         else:
             errors = form.errors
     return render_template("entry_form.html", form = form, errors = errors)
