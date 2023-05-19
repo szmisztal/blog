7d805612047319eb80dd4a365b862e803233071c
szmisztal <sz.misztal@gmail.com>
2023-05-19 13:15:54 +0200

    add username and password variable

--- config.py
+++ config.py
@@ -2,7 +2,10 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
+    ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "admin")
+    ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "password")
     SECRET_KEY = os.environ.get("SECRET_KEY") or "remember-to-add-secret-key"
     SQLALCHEMY_DATABASE_URI = (
         os.environ.get("DATABASE_URL") or
