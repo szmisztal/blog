ae9b96a3e0c9abef032d097ce746825d550952c3
szmisztal <sz.misztal@gmail.com>
2023-05-20 13:06:39 +0200

    add 'login_required' function

--- blog/routes.py
+++ blog/routes.py
@@ -2,6 +2,15 @@ from flask import render_template, request, redirect, url_for, flash, session
 from blog import app
 from blog.models import Entry, db
 from blog.forms import EntryForm, LoginForm
+import functools
+
+def login_required(view_func):
+    @functools.wraps(view_func)
+    def check_permissions(*args, **kwargs):
+        if session.get("logged_in"):
+            return view_func(*args, **kwargs)
+        return redirect(url_for('login', next = request.path))
+    return check_permissions
 
 @app.route("/")
 def base():
@@ -10,6 +19,7 @@ def base():
 
 @app.route("/new_post/", methods = ["GET", "POST"])
 @app.route("/edit_post/<int:entry_id>", methods = ["GET", "POST"])
+@login_required
 def create_or_edit_entry(entry_id = None):
     if entry_id:
         entry = Entry.query.filter_by(id = entry_id).first_or_404()
@@ -43,7 +53,7 @@ def login():
     next_url = request.args.get('next')
     if request.method == "POST":
         if form.validate_on_submit():
-            session["logged in"] = True
+            session["logged_in"] = True
             session.permanent = True
             flash("You are logged in.")
             return redirect(next_url or url_for('base'))
