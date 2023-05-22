8e5238de326a0ae91caea7a9c8c161df9fc64350
szmisztal <sz.misztal@gmail.com>
2023-05-22 12:56:49 +0200

    add migration db before app start

--- blog/__init__.py
+++ blog/__init__.py
@@ -10,6 +10,21 @@ migrate = Migrate(app, db)
 
 from blog import routes, models
 
+def db_init():
+    db.create_all()
+    migrate.stamp()
+
+@app.cli.command()
+def db_migrate():
+    migrate.init()
+    migrate.migrate()
+    migrate.upgrade()
+
+@app.before_first_request
+def before_first_request():
+    db_init()
+    db_migrate()
+
 @app.shell_context_processor
 def make_shell_context():
   return {
